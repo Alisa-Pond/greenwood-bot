82cd99ecb0ce742b0962652c1ab57b2e96f4980a
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-01 17:13:57 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,33 @@
+from telebot import types
 
+def get_main_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(types.KeyboardButton("🧙‍♂️ Персонаж"), types.KeyboardButton("🎒 Рюкзак"))
+    markup.row(types.KeyboardButton("📜 Основний квест"), types.KeyboardButton("🎯 Мої Квести"))
+    markup.row(types.KeyboardButton("➕ Додати Справу"))
+    return markup
+
+def get_quests_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(types.KeyboardButton("📜 Сувої завдань"), types.KeyboardButton("🔄 Щоденні ритуали"))
+    markup.row(types.KeyboardButton("🌱 Теплиця Грінвуду"))
+    markup.row(types.KeyboardButton("🔙 Назад"))
+    return markup
+
+def get_scrolls_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(types.KeyboardButton("➕ Створити сувой"), types.KeyboardButton("✅ Виконати завдання"))
+    markup.row(types.KeyboardButton("🔥 Спалити сувой"), types.KeyboardButton("🔙 Назад до квестів"))
+    return markup
+
+def get_rituals_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(types.KeyboardButton("➕ Створити ритуал"), types.KeyboardButton("✅ Виконати ритуал"))
+    markup.row(types.KeyboardButton("🔥 Спалити ритуал"), types.KeyboardButton("🔙 Назад до квестів"))
+    return markup
+
+def get_greenhouse_menu():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(types.KeyboardButton("🌱 Посадити насіння"), types.KeyboardButton("🌸 Квітка розквітла"))
+    markup.row(types.KeyboardButton("🪾 Вирвати баобаб"), types.KeyboardButton("🔙 Назад до квестів"))
+    return markup
