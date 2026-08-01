a262e83d1bc359b2327f0e4874cb03aa7a08c1fe
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-01 17:06:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
+import os
+import telebot
+from supabase import create_client, Client
 
+# Налаштування змінних оточення (з Render)
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise ValueError("❌ ПОМИЛКА: Не вказано SUPABASE_URL або SUPABASE_KEY у налаштуваннях Render!")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("❌ ПОМИЛКА: Render не передав BOT_TOKEN! Перевір налаштування Environment Variables!")
+
+bot = telebot.TeleBot(BOT_TOKEN)
