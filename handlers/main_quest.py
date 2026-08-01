2af3f80fbf7ca55290f043e3e03cdce67a344e64
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-01 17:20:21 +0300

    Update main_quest.py

--- handlers/main_quest.py
+++ handlers/main_quest.py
@@ -1,1 +1,5 @@
+from config import bot
 
+@bot.message_handler(func=lambda message: message.text == "📜 Основний квест")
+def main_quest_handler(message):
+    bot.send_message(message.chat.id, "🔒 <b>Основний квест заблоковано.</b>", parse_mode="HTML")
