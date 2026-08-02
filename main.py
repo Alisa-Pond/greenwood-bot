9b7a7fdb014323cb4d9b0eaf4516d94438cbbf6d
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-02 21:17:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     
     print("🚀 Запускаємо бота через Long Polling...")
     # infinity_polling автоматично перезапускає опитування при мережевих збоях
-    bot.infinity_polling(skip_pending_updates=True)
+    bot.infinity_polling()
