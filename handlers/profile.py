686b079d8aa7fa73ba6598880fa1d6fa86e123cb
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-02 17:55:52 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,7 +1,7 @@
 from services.config import bot
 import traceback
 from telebot import types
-from database import get_player, clean_skin_tones
+from services.database import get_player, clean_skin_tones
 from keyboards import get_main_menu
 print("⚙️ Реєструємо хендлер /start у profile.py...")
 @bot.message_handler(commands=['start'])
