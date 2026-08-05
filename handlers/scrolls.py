36957db22bd7d1d886e90d2c3e13cc551ddf63a9
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-05 14:08:49 +0300

    Update scrolls.py

--- handlers/scrolls.py
+++ handlers/scrolls.py
@@ -1,1 +1,13 @@
+import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+from telebot import types
+
+from services.config import bot
+from services.database import get_player, update_player
+from services.utils import clean_skin_tones
+
+from keyboards import get_scrolls_menu
+
+print("📜 Модуль handlers/scrolls завантажено!")
