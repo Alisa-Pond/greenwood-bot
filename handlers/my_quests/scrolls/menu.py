791ef4e88ab1019df827eeae3c5dc5fb67e4aacb
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-07 17:14:09 +0300

    Update menu.py

--- handlers/my_quests/scrolls/menu.py
+++ handlers/my_quests/scrolls/menu.py
@@ -1,1 +1,17 @@
+from services.config import bot
+from keyboards import get_scrolls_menu
 
+
+print("⚙️ Реєструємо меню сувоїв...")
+
+
+@bot.message_handler(func=lambda message: message.text == "📜 Сувої")
+def open_scrolls(message):
+
+    bot.send_message(
+        message.chat.id,
+        "📜 <b>Сувої</b>\n\n"
+        "Тут зберігаються всі твої одноразові справи та завдання.",
+        parse_mode="HTML",
+        reply_markup=get_scrolls_menu()
+    )
