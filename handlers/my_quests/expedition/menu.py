2853f1e427d8c7b7da37611f4ac9d386ff575200
Alisa-Pond <frostcoldgo@gmail.com>
2026-08-07 17:55:02 +0300

    Update menu.py

--- handlers/my_quests/expedition/menu.py
+++ handlers/my_quests/expedition/menu.py
@@ -1,5 +1,5 @@
 from services.config import bot
-from keyboards import get_expeditions_menu
+from keyboards import get_quests_menu, get_expeditions_menu
 
 
 print("⚙️ Реєструємо меню експедицій...")
@@ -14,4 +14,13 @@ def open_expeditions(message):
         "Цей розділ ще розробляється.",
         parse_mode="HTML",
         reply_markup=get_expeditions_menu()
+    )
+    
+@bot.message_handler(func=lambda m: m.text == "🔙 Назад до квестів")
+def back_from_expeditions(message):
+
+    bot.send_message(
+        message.chat.id,
+        "📝 Меню квестів",
+        reply_markup=get_quests_menu()
     )
